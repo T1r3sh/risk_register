d6cd4626411a06aff8c11e192e59243487d77e3d
t1r3sh <sadandsand@gmail.com>
2023-05-13 18:29:29 +0500

    dialogWindow

---
+++ backup/reportDialogWindow_ui.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'reportDialogWindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QPushButton, QSizePolicy,
+    QVBoxLayout, QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(594, 511)
+        self.verticalLayoutWidget = QWidget(Dialog)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(71, 36, 160, 205))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton_3 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.verticalLayout.addWidget(self.pushButton_3)
+
+        self.pushButton_5 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+
+        self.verticalLayout.addWidget(self.pushButton_5)
+
+        self.pushButton_6 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+
+        self.verticalLayout.addWidget(self.pushButton_6)
+
+        self.pushButton_4 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.verticalLayout.addWidget(self.pushButton_4)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"\u041c\u0430\u0442\u0440\u0438\u0446\u0430 \u0440\u0438\u0441\u043a\u043e\u0432", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+    # retranslateUi
+
